68c38b1692987f667f44535261edbdeeb466f882
Rıfat Arifoğlu <rifatarifoglu38@gmail.com>
2023-12-15 19:03:36 +0300

    Update CourseManager.java

--- files/CourseManager.java
+++ files/CourseManager.java
@@ -10,6 +10,44 @@ public class CourseManager {
     private static Scanner in = new Scanner(System.in);
 
     /**
+     * Create a {@code Course}
+     * @return Is the course created
+     */
+    public static boolean addCourse(){
+        int courseID = 0;
+        String name = "";
+        int quota = 0;
+        
+        String nextLine = "";
+        String format = "%-10s: ";
+
+        System.out.print(String.format(format, "ID"));
+        courseID = in.nextInt();
+        nextLine = in.nextLine();
+
+        if(StudentManagement.isStudentExist(courseID)){
+            System.out.println("This ID already exists!");
+            return false;
+        }
+
+        System.out.print(String.format(format, "Name"));
+        name = in.nextLine();
+
+        System.out.print(String.format(format, "Quota"));
+        quota = in.nextInt();
+        nextLine = in.nextLine();
+        
+        CourseManager.getCourseList().add(new Course(courseID, name, quota));
+
+        return true;
+    }
+
+    public boolean removeCourse(){
+        //TO-DO
+        return true;
+    }
+
+    /**
      * Checks if this course is already created.
      * @param s Course that going to get checked
      * @return  Is this course exists
