618a577f0d34736404b9eb2fcfe0b5423258e2aa
Rıfat Arifoğlu <rifatarifoglu38@gmail.com>
2023-12-16 14:04:36 +0300

    Update Main.java

--- files/Main.java
+++ files/Main.java
@@ -3,5 +3,9 @@ package files;
 public class Main{
     public static void main(String[] args) {
         
+        //Close the Scanners of managers
+        StudentManager.closeScanner();
+        CourseManager.closeScanner();
+        GradeManager.closeScanner();
     }
 }
